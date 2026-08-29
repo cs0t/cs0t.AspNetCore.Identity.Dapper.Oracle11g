525c59fb9b841a128736b9e4bced6a0f3b980625
cs0t <sabaodisharia04@gmail.com>
2026-08-29 20:59:54 +0400

    Modify application suer model, change guid to long identity key !

--- cs0t.AspNetCore.Identity.Dapper.Oracle11g/Models/ApplicationUser.cs
+++ cs0t.AspNetCore.Identity.Dapper.Oracle11g/Models/ApplicationUser.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Stores;
 using Microsoft.AspNetCore.Identity;
 
 namespace cs0t.AspNetCore.Identity.Dapper.Oracle11g.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<long>
     {
-        public int UserType { get; set; } 
-        public bool IsActive { get; set; } = true;
+        public string FirstName { get; set; } =  string.Empty;
+        public string LastName { get; set; } =  string.Empty;
+        public bool IsActive { get; set; } 
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime? LastLoggedInAtUtc { get; set; }
+        public DateTime? PasswordChangedAtUtc { get; set; }
 
-        internal List<Claim> Claims { get; set; }
-        internal List<UserRole> Roles { get; set; }
-        internal List<UserLoginInfo> Logins { get; set; }
-        internal List<UserToken> Tokens { get; set; }
+        public List<IdentityUserClaim<long>> Claims { get; set; } = [];
+        public List<IdentityUserRole<long>> Roles { get; set; } = [];
+        public List<IdentityUserLogin<long>> Logins { get; set; } = [];
+        public List<IdentityUserToken<long>> Tokens { get; set; } = [];
     }
 }
