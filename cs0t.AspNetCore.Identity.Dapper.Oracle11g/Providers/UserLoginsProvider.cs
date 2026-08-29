edc00461aef419fb8179d024898f9411259fbad4
cs0t <sabaodisharia04@gmail.com>
2026-08-30 00:09:14 +0400

    Add user logins oracle provider !

--- cs0t.AspNetCore.Identity.Dapper.Oracle11g/Providers/UserLoginsProvider.cs
+++ cs0t.AspNetCore.Identity.Dapper.Oracle11g/Providers/UserLoginsProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Models;
 using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Stores;
@@ -9,51 +10,88 @@ using Microsoft.AspNetCore.Identity;
 
 namespace cs0t.AspNetCore.Identity.Dapper.Oracle11g.Providers
 {
-    internal class UserLoginsProvider
+    internal class UserLoginsProvider(IDatabaseConnectionFactory databaseConnectionFactory)
     {
-        private readonly IDatabaseConnectionFactory _databaseConnectionFactory;
-
-        public UserLoginsProvider(IDatabaseConnectionFactory databaseConnectionFactory)
+        public async Task<IList<UserLoginInfo>> GetLoginsAsync(ApplicationUser user, CancellationToken ct = default) 
         {
-            _databaseConnectionFactory = databaseConnectionFactory;
-        }
+            user.ThrowIfNull(nameof(user));
 
-        public async Task<IList<UserLoginInfo>> GetLoginsAsync(ApplicationUser user) {
-            var command = "SELECT * " +
-                                   $"FROM [{_databaseConnectionFactory.DbSchema}].[AspNetUserLogins] " +
-                                   "WHERE UserId = @UserId;";
+            var command = $"""
+                           SELECT LoginProvider, ProviderKey, ProviderDisplayName 
+                           FROM {databaseConnectionFactory.Options.DbSchema}.{databaseConnectionFactory.Options.UserLoginsTableName} 
+                           WHERE UserId = :UserId
+                           """;
 
-            await using var sqlConnection = await _databaseConnectionFactory.CreateConnectionAsync();
-            return (
-                    await sqlConnection.QueryAsync<UserLogin>(command, new { UserId = user.Id })
-                )
+            await using var oracleConnection = await databaseConnectionFactory.CreateConnectionAsync(ct).ConfigureAwait(false);
+            
+            var result = await oracleConnection.QueryAsync<(string LoginProvider, string ProviderKey, string ProviderDisplayName)>(
+                new CommandDefinition(command, new { UserId = user.Id }, cancellationToken: ct)
+            ).ConfigureAwait(false);
+
+            return result
                 .Select(x => new UserLoginInfo(x.LoginProvider, x.ProviderKey, x.ProviderDisplayName))
                 .ToList();
         }
+        
+        public async Task<ApplicationUser?> FindByLoginAsync(string loginProvider, string providerKey, CancellationToken ct = default) 
+        {
+            if (string.IsNullOrWhiteSpace(loginProvider)) throw new ArgumentException("Login provider cannot be empty.", nameof(loginProvider));
+            if (string.IsNullOrWhiteSpace(providerKey)) throw new ArgumentException("Provider key cannot be empty.", nameof(providerKey));
 
-        public async Task<ApplicationUser> FindByLoginAsync(string loginProvider, string providerKey) {
-            string[] command =
-            {
-                "SELECT UserId " +
-                $"FROM [{_databaseConnectionFactory.DbSchema}].[AspNetUserLogins] " +
-                "WHERE LoginProvider = @LoginProvider AND ProviderKey = @ProviderKey;"
-            };
+            var command = $"""
+                           SELECT u.* 
+                           FROM {databaseConnectionFactory.Options.DbSchema}.{databaseConnectionFactory.Options.UsersTableName} u
+                           INNER JOIN {databaseConnectionFactory.Options.DbSchema}.{databaseConnectionFactory.Options.UserLoginsTableName} ul ON u.Id = ul.UserId
+                           WHERE ul.LoginProvider = :LoginProvider AND ul.ProviderKey = :ProviderKey
+                           """;
 
-            await using var sqlConnection = await _databaseConnectionFactory.CreateConnectionAsync();
-            var userId = await sqlConnection.QuerySingleOrDefaultAsync<Guid?>(command[0], new {
-                LoginProvider = loginProvider,
-                ProviderKey = providerKey
-            });
-
-            if (userId == null) {
-                return null;
-            }
-
-            command[0] = "SELECT * " +
-                         $"FROM [{_databaseConnectionFactory.DbSchema}].[AspNetUsers] " +
-                         "WHERE Id = @Id;";
-
-            return await sqlConnection.QuerySingleAsync<ApplicationUser>(command[0], new { Id = userId });
+            await using var oracleConnection = await databaseConnectionFactory.CreateConnectionAsync(ct).ConfigureAwait(false);
+            
+            return await oracleConnection.QuerySingleOrDefaultAsync<ApplicationUser>(
+                new CommandDefinition(command, new { LoginProvider = loginProvider, ProviderKey = providerKey }, cancellationToken: ct)
+            ).ConfigureAwait(false);
         }
+        
+        public async Task AddLoginAsync(ApplicationUser user, UserLoginInfo login, CancellationToken ct = default)
+        {
+            user.ThrowIfNull(nameof(user));
+            login.ThrowIfNull(nameof(login));
+            
+            var command = $"""
+                           INSERT INTO {databaseConnectionFactory.Options.DbSchema}.{databaseConnectionFactory.Options.UserLoginsTableName} 
+                           (UserId, LoginProvider, ProviderKey, ProviderDisplayName) 
+                           VALUES (:UserId, :LoginProvider, :ProviderKey, :ProviderDisplayName)
+                           """;
+
+            await using var oracleConnection = await databaseConnectionFactory.CreateConnectionAsync(ct).ConfigureAwait(false);
+            
+            await oracleConnection.ExecuteAsync(
+                new CommandDefinition(command, new { 
+                    UserId = user.Id, 
+                    LoginProvider = login.LoginProvider, 
+                    ProviderKey = login.ProviderKey, 
+                    ProviderDisplayName = login.ProviderDisplayName 
+                }, cancellationToken: ct)
+            ).ConfigureAwait(false);
+        }
+        
+        public async Task RemoveLoginAsync(ApplicationUser user, string loginProvider, string providerKey, CancellationToken ct = default)
+        {
+            user.ThrowIfNull(nameof(user));
+            if (string.IsNullOrWhiteSpace(loginProvider)) throw new ArgumentException("Login provider cannot be empty.", nameof(loginProvider));
+            if (string.IsNullOrWhiteSpace(providerKey)) throw new ArgumentException("Provider key cannot be empty.", nameof(providerKey));
+
+            var command = $"""
+                           DELETE FROM {databaseConnectionFactory.Options.DbSchema}.{databaseConnectionFactory.Options.UserLoginsTableName} 
+                           WHERE UserId = :UserId AND LoginProvider = :LoginProvider AND ProviderKey = :ProviderKey
+                           """;
+
+            await using var oracleConnection = await databaseConnectionFactory.CreateConnectionAsync(ct).ConfigureAwait(false);
+            
+            await oracleConnection.ExecuteAsync(
+                new CommandDefinition(command, new { UserId = user.Id, LoginProvider = loginProvider, ProviderKey = providerKey }, cancellationToken: ct)
+            ).ConfigureAwait(false);
+        }
+
     }
 }
