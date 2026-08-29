37252f1f0d95211d2afafa274bb73e9d55df8fdc
cs0t <sabaodisharia04@gmail.com>
2026-08-29 20:58:16 +0400

    Remove old POCO helpers for identity classes (claims, roleclaims, logins, tokens ), replace them with actual identity classes !

--- cs0t.AspNetCore.Identity.Dapper.Oracle11g/Stores/UserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace cs0t.AspNetCore.Identity.Dapper.Oracle11g.Stores
-{
-    internal class UserRole
-    {
-        public string RoleId { get; set; }
-        public string RoleName { get; set; }
-    }
-}
