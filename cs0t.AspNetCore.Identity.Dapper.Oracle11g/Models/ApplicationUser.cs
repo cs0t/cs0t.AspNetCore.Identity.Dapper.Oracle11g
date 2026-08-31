22ab7908620b9e420f7509d0ceadd01bed98a2b8
cs0t <sabaodisharia04@gmail.com>
2026-08-31 16:02:33 +0400

    Change application user model tu support new hybtid tracking strategy !

--- cs0t.AspNetCore.Identity.Dapper.Oracle11g/Models/ApplicationUser.cs
+++ cs0t.AspNetCore.Identity.Dapper.Oracle11g/Models/ApplicationUser.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Claims;
-using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Stores;
 using Microsoft.AspNetCore.Identity;
 
 namespace cs0t.AspNetCore.Identity.Dapper.Oracle11g.Models
@@ -15,9 +13,9 @@ namespace cs0t.AspNetCore.Identity.Dapper.Oracle11g.Models
         public DateTime? LastLoggedInAtUtc { get; set; }
         public DateTime? PasswordChangedAtUtc { get; set; }
 
-        public List<IdentityUserClaim<long>> Claims { get; set; } = [];
-        public List<IdentityUserRole<long>> Roles { get; set; } = [];
-        public List<IdentityUserLogin<long>> Logins { get; set; } = [];
-        public List<IdentityUserToken<long>> Tokens { get; set; } = [];
+        public List<IdentityUserClaim<long>>? Claims { get; set; }
+        public List<ApplicationUserRole>? Roles { get; set; } 
+        public List<IdentityUserLogin<long>>? Logins { get; set; }
+        public List<IdentityUserToken<long>>? Tokens { get; set; }
     }
 }
