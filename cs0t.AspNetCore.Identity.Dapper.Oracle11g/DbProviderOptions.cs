fa46c0a21fa10f8232532cf1f8d4b484d2f1228e
cs0t <sabaodisharia04@gmail.com>
2026-09-03 17:46:38 +0400

    Chenge options static names to match overall style

--- cs0t.AspNetCore.Identity.Dapper.Oracle11g/DbProviderOptions.cs
+++ cs0t.AspNetCore.Identity.Dapper.Oracle11g/DbProviderOptions.cs
@@ -9,8 +9,8 @@
         public string UsersTableName { get; set; } = "ASPNET_USERS";
         public string RolesTableName { get; set; } =  "ASPNET_ROLES";
         public string UserRolesTableName { get; set; }  = "ASPNET_USER_ROLES";
-        public string UserClaimsTable { get; set; } = "ASPNET_USER_CLAIMS";
-        public string UserRoleClaimsTable { get; set; } =  "ASPNET_USER_ROLE_CLAIMS";
+        public string UserClaimsTableName { get; set; } = "ASPNET_USER_CLAIMS";
+        public string UserRoleClaimsTableName { get; set; } =  "ASPNET_USER_ROLE_CLAIMS";
         public string UserLoginsTableName { get; set; } =  "ASPNET_USER_LOGINS";
         public string UserTokensTableName { get; set; } =   "ASPNET_USER_TOKENS";
         
