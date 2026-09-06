374ce9d461b4f02482825c442637081e1c6019a4
cs0t <sabaodisharia04@gmail.com>
2026-09-06 23:13:25 +0400

    Change oracle docker ficture from class fixture to assembly fixture !

---
+++ cs0t.AspNetCore.Identity.Dapper.Oracle11g.Tests/Infrastructure/OracleDatabaseCollectionFixture.cs
@@ -0,0 +1,7 @@
+namespace cs0t.AspNetCore.Identity.Dapper.Oracle11g.Tests.Infrastructure;
+
+[CollectionDefinition(nameof(OracleDatabaseCollectionFixture))]
+public class OracleDatabaseCollectionFixture : ICollectionFixture<OracleDockerFixture>
+{
+    
+}
