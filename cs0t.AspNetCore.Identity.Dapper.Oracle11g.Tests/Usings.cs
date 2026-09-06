b5428bc5bfa6b8c2e93ad48d64c5a0095a06719b
cs0t <sabaodisharia04@gmail.com>
2026-09-06 21:54:01 +0400

    Add global usings in tests project !

---
+++ cs0t.AspNetCore.Identity.Dapper.Oracle11g.Tests/Usings.cs
@@ -0,0 +1,9 @@
+global using System.Security.Claims;
+global using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Stores;
+global using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Tests.Infrastructure;
+global using Dapper;
+global using Microsoft.AspNetCore.Identity;
+global using System.Data;
+global using cs0t.AspNetCore.Identity.Dapper.Oracle11g.Models;
+global using Oracle.ManagedDataAccess.Client;
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
